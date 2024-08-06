c912a87174456689c8a2ffbf6b9fbc2f95420dbc
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-05 22:36:54 -0700

    Added a PriceQuotation model

---
+++ PriceQuotationApp/Models/PriceQuotation.cs
@@ -0,0 +1,29 @@
+﻿namespace PriceQuotationApp.Models
+{
+    /// <summary>
+    /// This class represents a price quotation.
+    /// </summary>
+    public class PriceQuotation
+    {
+        /// <summary>
+        /// This number uses the value the user inputs
+        /// </summary>
+        public decimal Subtotal { get; set; }
+
+        /// <summary>
+        /// This value is obtained from the user input
+        /// </summary>
+        public decimal DiscountPercent { get; set; }
+
+        /// <summary>
+        /// This value is calculated by multiplying the subtotal by the discount percent
+        /// </summary>
+        public decimal DiscountAmount => Subtotal * (DiscountPercent / 100);
+
+        /// <summary>
+        /// This value is calculated by subtracting the discount amount from the subtotal
+        /// </summary>
+        public decimal Total => Subtotal - DiscountAmount;
+
+    }
+}
